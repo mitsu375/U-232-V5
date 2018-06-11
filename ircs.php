3a5da3090eed7320f5f1b8fcf4c6990f325d5050
mitsu375 <38742598+mitsu375@users.noreply.github.com>
2018-06-10 20:08:22 -0400

    Update ircs.php

--- ircs.php
+++ ircs.php
@@ -21,8 +21,8 @@
 require_once (__DIR__ . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'bittorrent.php');
 require_once (INCL_DIR . 'user_functions.php');
 require_once (INCL_DIR . 'function_onlinetime.php');
-$password = ""; // same as in staff.tcl;
-$hash = ""; // same as in staff.tcl;
+$password = "adlsadladadll"; // same as in staff.tcl;
+$hash = "adlsadladadll"; // same as in staff.tcl;
 $modclass = "4"; // minumum staff class;
 function calctime($val)
 {
